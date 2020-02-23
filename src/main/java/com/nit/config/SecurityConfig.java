897bbfb0feeb8dc50752b6464375e9e1832bf01b
shridhar <shridharmali88@gmail.com>
2020-02-23 13:21:23 +0530

    All Functionality Working Fine

--- src/main/java/com/nit/config/SecurityConfig.java
+++ src/main/java/com/nit/config/SecurityConfig.java
@@ -1,5 +1,46 @@
 package com.nit.config;
 
-public class SecurityConfig {
+import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	@Autowired
+	private DataSource ds;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		System.out.println("SecurityConfig.configure()");
+		//set configuration to Authentication Provider
+		auth.jdbcAuthentication()
+			.dataSource(ds)
+			.usersByUsernameQuery("SELECT UNAME,PWD,STATUS FROM USERLIST WHERE UNAME=?")
+			.authoritiesByUsernameQuery("SELECT UNAME,ROLE FROM USER_ROLES WHERE UNAME=?");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		System.out.println("SecurityConfig.configure()");
+		//set Configuration To Http
+		http.authorizeRequests()
+			.antMatchers("/home.htm").access("permitAll")
+			.antMatchers("/wish.htm").access("hasAnyRole('ROLE_ADMIN','ROLE_FACULTY')")
+			.and()
+			.formLogin()
+			.and()
+			.logout().logoutSuccessUrl("/logout_success.jsp")
+			.and()
+            .exceptionHandling().accessDeniedPage("/access_denied.jsp")
+			.and()
+			.rememberMe()
+			.and()
+			.sessionManagement().maximumSessions(3).maxSessionsPreventsLogin(true);
+	}
 }
